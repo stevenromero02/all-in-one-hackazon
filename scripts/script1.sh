d2f3aa01d663194844df99c9a3fc174bd5656914
stevenromero02 <60715750+stevenromero02@users.noreply.github.com>
2020-02-18 14:04:22 -0800

    Create script1.sh

---
+++ scripts/script1.sh
@@ -0,0 +1,13 @@
+#------------
+#!/bin/bash
+# 
+
+if [ SECRET = 1 ]
+then
+cp /vunerabilities/VULNERABILITIESv1.md /var/www/hackazon/VULNERABILITIES.md
+fi
+
+if [ SECRET = 2 ]
+then
+cp /vunerabilities/VULNERABILITIESv2.md /var/www/hackazon/VULNERABILITIES.md
+fi
