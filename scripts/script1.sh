2ed4f5f410080edbc5bdf7ac22fee708328777f5
stevenromero02 <60715750+stevenromero02@users.noreply.github.com>
2020-02-20 10:50:27 -0800

    Update script1.sh

--- scripts/script1.sh
+++ scripts/script1.sh
@@ -1,11 +1,25 @@
-#!/bin/bash 
+#!/bin/bash
+# 
 
-if [ SECRET = 1 ]
+SECRET2_VALUE = RAND_INT
+echo SECRET2_VALUE
+
+if [ SECRET2_VALUE = 1 ]
 then
 cp /vunerabilities/VULNERABILITIESv1.md /var/www/hackazon/VULNERABILITIES.md
 fi
 
-if [ SECRET = 2 ]
+if [ SECRET2_VALUE = 2 ]
 then
 cp /vunerabilities/VULNERABILITIESv2.md /var/www/hackazon/VULNERABILITIES.md
+fi
+
+if [ SECRET2_VALUE = 3 ]
+then
+cp /vunerabilities/VULNERABILITIESv3.md /var/www/hackazon/VULNERABILITIES.md
+fi
+
+if [ SECRET2_VALUE = 4 ]
+then
+cp /vunerabilities/VULNERABILITIESv4.md /var/www/hackazon/VULNERABILITIES.md
 fi
