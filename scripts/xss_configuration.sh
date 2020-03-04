d89d1b1c1a70f87456d58abae2b4c0510ade73fa
stevenromero02 <60715750+stevenromero02@users.noreply.github.com>
2020-03-04 06:18:02 -0800

    fixed spelling VUL

--- scripts/xss_configuration.sh
+++ scripts/xss_configuration.sh
@@ -7,120 +7,120 @@ cp /vunerabilities/xss_VULNERABILITIES.md /var/www/hackazon/VULNERABILITIES.md
 
 if [ SECRET2_VALUE=1 ]
 then
-	cp /vunerabilities/xss_accountv1.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv1.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv1.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv4.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv2.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist3.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv1.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv1.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv1.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv4.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv2.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist3.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=2 ]
 then
-	cp /vunerabilities/xss_accountv2.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv3.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv2.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv3.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv1.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv1.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv2.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv3.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv2.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv3.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv1.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv1.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=3 ]
 then
-	cp /vunerabilities/xss_accountv3.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv2.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv3.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv2.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv3.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist2.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv3.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv2.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv3.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv2.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv3.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist2.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=4 ]
 then
-	cp /vunerabilities/xss_accountv4.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv4.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv1.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv1.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv2.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist3.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv4.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv4.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv1.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv1.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv2.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist3.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=5 ]
 then
-	cp /vunerabilities/xss_accountv5.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv1.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv2.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv1.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv1.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv5.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv1.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv2.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv1.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv1.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=6 ]
 then
-	cp /vunerabilities/xss_accountv1.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv2.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv3.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv2.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv3.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv2.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv1.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv2.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv3.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv2.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv3.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv2.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=7 ]
 then
-	cp /vunerabilities/xss_accountv2.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv4.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv1.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv3.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv2.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv2.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv2.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv4.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv1.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv3.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv2.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv2.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=8 ]
 then
-	cp /vunerabilities/xss_accountv3.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv3.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv2.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv4.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv1.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv3.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv3.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv2.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv4.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv1.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv3.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist1.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=9 ]
 then
-	cp /vunerabilities/xss_accountv4.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv2.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv3.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv2.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv3.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv1.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist2.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv4.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv2.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv1.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv3.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv2.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv3.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv1.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist2.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
 
 if [ SECRET2_VALUE=10 ]
 then
-	cp /vunerabilities/xss_accountv5.php /var/www/hackazon/assets/config/vuln/account.php
-	cp /vunerabilities/xss_contactv3.php /var/www/hackazon/assets/config/vuln/contact.php
-	cp /vunerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
-	cp /vunerabilities/xss_cartv1.php /var/www/hackazon/assets/config/vuln/cart.php
-	cp /vunerabilities/xss_checkoutv3.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_faqv2.php /var/www/hackazon/assets/config/vuln/faq.php
-	cp /vunerabilities/xss_userv2.php /var/www/hackazon/assets/config/vuln/checkout.php
-	cp /vunerabilities/xss_wishlist3.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_accountv5.php /var/www/hackazon/assets/config/vuln/account.php
+	cp /vulnerabilities/xss_contactv3.php /var/www/hackazon/assets/config/vuln/contact.php
+	cp /vulnerabilities/xss_amfv2.php /var/www/hackazon/assets/config/vuln/amf.php
+	cp /vulnerabilities/xss_cartv1.php /var/www/hackazon/assets/config/vuln/cart.php
+	cp /vulnerabilities/xss_checkoutv3.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_faqv2.php /var/www/hackazon/assets/config/vuln/faq.php
+	cp /vulnerabilities/xss_userv2.php /var/www/hackazon/assets/config/vuln/checkout.php
+	cp /vulnerabilities/xss_wishlist3.php /var/www/hackazon/assets/config/vuln/faq.php
 fi
